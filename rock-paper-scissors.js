300664a660ded28233f5d85da3a15dd0029e5383
vadimkrue <vadimkrue@gmail.com>
2024-10-24 11:24:04 +0500

    Write game logic and describe steps with pseudocode
    The file now contains the described game logic.
    It also contains the steps to be completes to make
    the game work.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,40 @@
+// BASIC LOGIC
+
+// We need to write a game rock-paper-scissors in a browser console
+// — Participants: human and a computer
+// — The person will choose an option (r/p/s), the computer will randomize choose its option
+// — The winner is determined as follows:
+//      Rock beats scissors
+//      scissors beat paper
+//      and paper beats rock
+//  If the outcomes are the same, it's a tie
+
+
+
+// STEPS
+
+// [First] ROUND STARTS
+//      Human enters an option (rock/paper/scissors)
+//      The computer set it's option
+//      Computer compares options
+//      Computer defines winner based on this rules:
+//              Rock beats scissors
+//              scissors beat paper
+//              and paper beats rock
+//              * if results are the same it's a tie 
+//      The comptuter announces ROUND RESULTS 
+//              eather human win or loose or if it's a tie (and stores this result)
+// [First] ROUND ENDS
+
+// [Second] ROUND STARTS ... [Second] ROUND ENDS
+
+// [Third] ROUND STARTS ... [Third] ROUND ENDS
+
+
+// THE RESULTS
+// Computer defines winner (2 or 3 wins) and looser (1 or 0 wins) or a tie (1 human w / tie / 1 computer w).
+//      Computer does that by comparing results of the rounds
+// Comptuter announces GAME RESULTS 
+//      eather human win or loose or if it's a tie
+
+// THE GAME STARTS AGAIN
