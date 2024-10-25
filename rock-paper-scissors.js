9fe83ae1dc12596f1a9ce547caeca7b52d6c8ee6
vadimkrue <vadimkrue@gmail.com>
2024-10-25 14:19:23 +0500

    Fix playRound logic
    The sequence was messed up. The values of the
    getHumanChoice and getComputerChoice functions
    were not updated at the beginning of the round.
    Therefore, the logic of the game were broken.
    
    Now sequence is right. The computer and the person
    enter the options, the options are compared and
    the result of the round is shown. After that,
    the next round begins with the same sequence.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,29 +6,59 @@ let humanScore = 0;
 let computerScore = 0; 
 
 function getHumanChoice () {
-	let input = prompt('Make a choice:\n\n— rock (type "r")\n— paper (type "p")\n— scissors (type "s")', '');
+	let humanInput = prompt('Make a choice:\n\n— rock (type "r")\n— paper (type "p")\n— scissors (type "s")', '');
 
-	let result = input.toLowerCase();
+	let humanOutput = humanInput.toLowerCase();
 
-	if (result === 'r' || result === 'rock') {
+	if (humanOutput === 'r' || humanOutput === 'rock') {
 		return(ROCK);
-	} else if (result === 'p' || result === 'paper') {
+	} else if (humanOutput === 'p' || humanOutput === 'paper') {
 		return(PAPER);
-	} else if (result === 's' || result === 'scissors') {
+	} else if (humanOutput === 's' || humanOutput === 'scissors') {
 		return(SCISSORS);
 	} else {
-		return;
+		console.log('Что-то не понятное')
+		return undefined;
 	}
 }
 
 function getComputerChoice() {
-	let result = Math.random();
-  
-	if (result <= 0.33) {
-	  return(ROCK);
-	} else if (result <= 0.66) {
-	  return(PAPER);
-	} else if (result <= 0.99) {
-	  return(SCISSORS);
+	let  computerInput = Math.random();
+
+	if (computerInput <= 0.33) {
+	computerOutput = ROCK;
+	} else if (computerInput <= 0.66) {
+	computerOutput = PAPER;
+	} else if (computerInput <= 0.99) {
+	computerOutput = SCISSORS;
+	}
+	return(computerOutput);
+  }
+
+function playRound () {
+	let humanSelection = getHumanChoice();
+	let computerSelection = getComputerChoice();
+
+	alert(`Human chose ${humanSelection} \nComputer chose ${computerSelection}`);
+
+	if ((humanSelection === ROCK && computerSelection === SCISSORS) 
+		|| (humanSelection === PAPER && computerSelection === ROCK)
+		|| (humanSelection === SCISSORS && computerSelection === PAPER)) {
+		humanScore += 1;
+		alert('round: You won this piece of hardware!');
+	
+	} else if (humanSelection === computerSelection) {
+		humanScore += 1;
+		computerScore += 1;
+		alert('round: It\'s a tie');
+	
+	} else {
+		computerScore += 1;
+		alert('round: Bad luck this time. You\'ve lost');
 	}
   }
+
+playRound();
+alert(`Human: ${humanScore} \nComputer: ${computerScore}`);
+playRound();
+alert(`Human: ${humanScore} \nComputer: ${computerScore}`);
